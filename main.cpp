c6710feb436fbfadb9ac3ce6f35993bf38594169
Apolline Crespel <apolline@MacBook-de-Apolline.local>
2017-12-16 14:22:28 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s;
+    std::cout << "Hello, World!" << std::endl;
+    std::cin >> s;
+    std::cout << "your string is: " << s << std::endl;
+    return 0;
+}
